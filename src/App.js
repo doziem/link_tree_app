3496352939149d7d7c262988ad58e0252c91f2bc
Doziem <agbokennethchidoziem@gmail.com>
2022-10-24 01:13:45 +0100

    finished the app setup and installed the needed packages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './pages/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
