7ae7ef3a32b84520b49d27cbb8bb76393ae380c7
Doziem <agbokennethchidoziem@gmail.com>
2022-10-25 20:18:19 +0100

    finished adding media query and propperly redirecting the Links on click

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ link }) => {
 
-    return <button className='link' ><a rel="noreferrer" id={link.id} target="_blank" href={link.link}>{link.name}</a></button>
+    return <button className='link'><a rel="noreferrer" id={link.id} target="_blank" href={link.link}>{link.name}</a></button>
 
 }
 
